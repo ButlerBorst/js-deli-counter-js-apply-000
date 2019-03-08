b156164d4c4fac7260d24422bfb99bbc624b9dde
Alex Borst <butler.borst@gmail.com>
2019-03-08 02:43:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var line = [];
+
+function takeANumber(line, name) {
+  
+  line.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${line.length} in line.`);
+  
+}
+
+
+function nowServing(katzDeliLine) {
+ let i = 0;
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+ if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeliLine.shift()}.`);
+}
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
